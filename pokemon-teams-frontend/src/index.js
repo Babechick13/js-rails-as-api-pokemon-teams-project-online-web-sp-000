16fc05b2c76b55f1e503515a6e9622f8f08234c6
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-30 04:53:01 +0000

    Automatically backed up by Learn

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,7 +3,31 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 
-
+#=> Example Request
+GET /trainers
+ 
+#=> Example Response
+[
+  {
+    "id":1,
+    "name":"Prince",
+    "pokemons":[
+      {
+        "id":140,
+        "nickname":"Jacey",
+        "species":"Kakuna",
+        "trainer_id":1
+      },
+      {
+        "id":141,
+        "nickname":"Zachariah",
+        "species":"Ditto",
+        "trainer_id":1
+      },
+      // ...
+    ]
+  }
+  // ..
 
 
 
